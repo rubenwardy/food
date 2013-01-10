ebb81cce88fbcbf9816587a5ba544fb8e5cf1fb1
Andrew Ward <anjayward@gmail.com>
2013-01-10 21:15:30 +0000

    Update support.lua

--- support.lua
+++ support.lua
@@ -9,6 +9,12 @@
 -- ======================================
 
 print "Food [Support] - Initialising"
+
+--NODE_IMPLEMENT() Gets an item from another mod softly
+-- modname: the name of the mod that the item will be got from
+-- n_ext: the name of the item that we want to get
+-- n_int: the name we want to save the item so we can load it as an ingredient
+-- resultfunc: if the mod does not exist, then do this function
 function node_implement(modname,n_ext,n_int,resultfunc)
          if not minetest.get_modpath(modname) then
             -- Mod is NOT installed
