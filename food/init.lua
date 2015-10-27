1724db4fba5e9cd87a0d1580c6e2d9869b32ec40
rubenwardy <rubenwardy@gmail.com>
2015-10-27 13:47:31 +0000

    Stop crash on damage disabled.

--- food/init.lua
+++ food/init.lua
@@ -174,9 +174,9 @@ function food.item_eat(amt)
 	elseif minetest.get_modpath("hud") and global_exists("hud") and hud.item_eat then
 		return hud.item_eat(amt)
 	elseif minetest.get_modpath("hbhunger") then
-		if global_exists("hbhunger") then
+		if global_exists("hbhunger") and hbhunger.item_eat then
 			return hbhunger.item_eat(amt)
-		elseif global_exists("hunger") then
+		elseif global_exists("hunger") and hunger.item_eat then
 			-- For backwards compatibility
 			-- It used to be called `hunger` rather than `hbhunger`
 			return hunger.item_eat(amt)
